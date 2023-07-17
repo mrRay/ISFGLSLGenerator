dbb331c3409edf213cfcbe0a67e74ab6bcd87ef2
ray cutler <ray@vidvox.net>
2023-07-17 10:04:33 -0400

    Cleans up and simplifies VVISF::Timestamp

--- libISFGLSLGenerator/include/VVISF_Base.hpp
+++ libISFGLSLGenerator/include/VVISF_Base.hpp
@@ -131,60 +131,27 @@ inline const std::string GLVersionToString(const GLVersion & v)	{ switch (v) { c
 
 
 
-//! Struct describing a timevalue as the quotient of two numbers, a value (64-bit unsigned int) and a scale factor (32-bit int).
-
-using RawTSClockType = std::chrono::steady_clock;
-using RawTSDuration = std::chrono::microseconds;
-using RawTSTime = std::chrono::time_point<RawTSClockType, RawTSDuration>;
-
-using DoubleTimeDuration = std::chrono::duration<double, std::ratio<1>>;
-using DoubleTimeTime = std::chrono::time_point<RawTSClockType, DoubleTimeDuration>;
-
+//! Struct describing a timevalue as a std::chrono::time_point with a monotonic microsecond-accurate timer
 struct Timestamp	{
 	
+	using RawTSClockType = std::chrono::steady_clock;
+	using RawTSDuration = std::chrono::microseconds;
+	using RawTSTime = std::chrono::time_point<RawTSClockType, RawTSDuration>;
+	
+	using DoubleTimeDuration = std::chrono::duration<double, std::ratio<1>>;
+	using DoubleTimeTime = std::chrono::time_point<RawTSClockType, DoubleTimeDuration>;
 	
 	//!	Creates a new Timestamp with the current time
-	Timestamp()	{
-		rawTime = std::chrono::time_point_cast<RawTSDuration>(RawTSClockType::now());
-	}
-	//!	Creates a new Timestamp with the passed time as a chrono::time_point< chrono::steady_clock, chrono::microseconds> >
-	Timestamp(const RawTSTime & inRawTime)	{
-		rawTime = inRawTime;
-	}
-	//!	Creates a new Timestamp with the passed time as a chrono::microseconds
-	Timestamp(const RawTSDuration & inRawDuration)	{
-		rawTime = RawTSTime(inRawDuration);
-	}
-	//!	Creates a new Timestamp with the passed time in seconds as a double
-	Timestamp(const double & inTimeInSeconds)	{
-		rawTime = std::chrono::time_point_cast<RawTSDuration>( DoubleTimeTime( DoubleTimeDuration(inTimeInSeconds) ) );
-	}
-	//!	Creates a new Timestamp expressed as the quotient of the two passed numbers (the number of frames divided by the number of seconds in which they occur)
-	Timestamp(const int & inFrameCount, const int & inSecondsCount)	{
-		if (inSecondsCount == 0)
-			rawTime = std::chrono::time_point_cast<RawTSDuration>( DoubleTimeTime( DoubleTimeDuration( double(0.0) ) ) );
-		else
-			rawTime = std::chrono::time_point_cast<RawTSDuration>( DoubleTimeTime( DoubleTimeDuration( double(inFrameCount)/double(inSecondsCount) ) ) );
-	}
-
+	Timestamp() : rawTime( std::chrono::time_point_cast<RawTSDuration>(RawTSClockType::now()) )	{}
 	
-	//!	Calculates the frame index of the receiver with the passed FPS, expressed as the quotient of two integers.
-	inline double frameIndexForFPS(const int & inFrameCount, const int & inSecondsCount)	{
-		RawTSTime		tmpTime(RawTSDuration(rawTime.time_since_epoch().count() * inFrameCount / inSecondsCount));
-		return std::chrono::time_point_cast<DoubleTimeDuration>(tmpTime).time_since_epoch().count();
-	}
-	//!	Calculates the frame index of the receiver with the passed FPS.
-	inline double frameIndexForFPS(const double & inFPS)	{
-		return frameIndexForFPS(int(inFPS * 1000000), 1000000);
-	}
-	//!	Calls 'frameIndexForFPS()' and then rounds the result before returning it
-	inline int64_t nearestFrameIndexForFPS(const int & inFrameCount, const int & inSecondsCount)	{
-		return int64_t(round(frameIndexForFPS(inFrameCount, inSecondsCount)));
-	}
-	//!	Calls 'frameIndexForFPS()' and then rounds the result before returning it
-	inline int64_t nearestFrameIndexForFPS(const double & inFPS)	{
-		return nearestFrameIndexForFPS(int(inFPS * 1000000), 1000000);
-	}
+	//!	Creates a new Timestamp with the passed time as a chrono::time_point< chrono::steady_clock, chrono::microseconds> >
+	Timestamp(const RawTSTime & inRawTime) : rawTime(inRawTime)	{}
+	
+	//!	Creates a new Timestamp with the passed time as a chrono::microseconds
+	Timestamp(const RawTSDuration & inRawDuration) : rawTime( RawTSTime(inRawDuration) )	{}
+	
+	//!	Creates a new Timestamp with the passed time in seconds as a double
+	Timestamp(const double & inTimeInSeconds) : rawTime( std::chrono::time_point_cast<RawTSDuration>( DoubleTimeTime( DoubleTimeDuration(inTimeInSeconds) ) ) ) {}
 	
 	
 	//!	Calculates and returns the receivers time in seconds, expressed as a double.
